4570c907c29c40a2f86abbf3788d9736a8237a5b
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2021-05-21 17:12:59 +0100

    initial commit

---
+++ Main.scala
@@ -0,0 +1,37 @@
+import akka.actor._
+import akka.stream._
+import akka.stream.scaladsl._
+
+import scala.util.Random
+import scala.concurrent._
+import scala.concurrent.duration._
+
+object Main extends App {
+  implicit val system = ActorSystem()
+  // implicit val mat = ActorMaterializer()
+
+  def flow = Flow[Int]
+    // .async
+    .map { n =>
+      val n = Random.nextInt(10)
+      println("producing " + n)
+      if (n == 5) throw new RuntimeException(s"Boom! Bad value found: $n")
+      else n.toString
+    }
+
+  val restartFlow = RestartFlow
+    .withBackoff(1.second, 2.seconds, 0.5)(() => flow)
+    
+  val full = Source(0 to 10)
+    .via(restartFlow)
+    .runWith(Sink.foreach(println))
+
+  import system.dispatcher
+  full
+    .andThen{
+      case res =>
+        println("Finished " + res)
+        system.terminate()
+        System.exit(0)
+    }
+}
---
+++ build.sbt
@@ -0,0 +1,7 @@
+
+scalaVersion := "2.13.5"
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % "2.6.14",
+  "com.typesafe.akka" %% "akka-stream" % "2.6.14"
+)
